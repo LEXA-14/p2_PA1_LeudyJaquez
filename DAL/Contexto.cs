24988a5136dd761ef425208977ea4c933f268bb3
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2025-10-27 18:50:55 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using p2_PA1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace p2_PA1_P2.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Registro> Registro { get; set; }
+}
---
+++ Models/Registro.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace p2_PA1_P2.Models;
+
+public class Registro
+{
+    [Key]
+
+    public int Id { get; set; }
+    [Required(ErrorMessage ="Este campo es requerido")]
+    public DateTime Fecha=DateTime.Now;
+
+    public int Cantidad { get; set; }
+}
