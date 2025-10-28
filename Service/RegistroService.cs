f0bb375917f42c092acbe8900854d956385f834a
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2025-10-27 20:42:54 -0400

    create, inicio del picker

--- Service/RegistroService.cs
+++ Service/RegistroService.cs
@@ -7,7 +7,29 @@ namespace p2_PA1_P2.Service;
 
 public class RegistroService(IDbContextFactory<Contexto> dbFactory)
 {
+    public async Task<bool> Guardar(Registro registro)
+    {
+        if (!await Existe(registro.Nombre))
+        {
+            return await Insertar(registro);
 
+        }
+        else
+        {
+            return false;
+        }
+    }
+    private async Task<bool> Insertar( Registro aportes)
+    {
+        await using var contexto = await dbFactory.CreateDbContextAsync();
+        contexto.Add(aportes);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+    private async Task<bool> Existe(string nombre)
+    {
+        await using var contexto = await dbFactory.CreateDbContextAsync();
+        return await contexto.Registro.AnyAsync(a => a.Nombre.ToLower() == nombre.ToLower());
+    }
 
     //Listar 
 
